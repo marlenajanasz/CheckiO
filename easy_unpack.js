8bcf0b0120c65580b5d949d0b323790e86fa4058
Marlena Janasz <mar.janasz87@gmail.com>
2020-05-03 15:26:58 +0200

    Update easy_unpack.js

--- easy_unpack.js
+++ easy_unpack.js
@@ -1,13 +1,11 @@
 //create a function that gets an array and returns a tuple with 3 elements:
 //the first, third and second element from the last for the given array
-//1
+
+//1 - best solution
 
 const easyUnpack = els => [els[0], els[2], els[els.length - 2]];
 
-
-easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]); // tu zwraca 8 x indeks 0 -> [1, 1, 1, 1, 1, 1, 1, 1]
-
-//2
+//2 - my solution
 
 function easyUnpack(arr){
 	let newArr = [];
@@ -15,22 +13,4 @@ function easyUnpack(arr){
 	return newArr;
 }
 
-easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]);
 
-//3 - wersja z 1) tylko nie strzałkowo
-
-function easyUnpack(els){
-	console.log([els[0], els[2], els[els.length-2]]);
-}
-
-easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]);
-
-// You should return a given string in reverse order
-function backwardString(str){
-	let arr = [];
-	for(let i=str.length-1; i>=0; i--){
-		arr.push(str[i]);	
-	}
-return arr.join("");
-}
-backwardString("val");
