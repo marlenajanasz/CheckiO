7933effce19edff41ad8224a54ca12231afe61e6
Marlena Janasz <mar.janasz87@gmail.com>
2020-05-10 21:55:57 +0200

    Update between_markers.js

--- between_markers.js
+++ between_markers.js
@@ -6,23 +6,19 @@ function betweenMarkers(text, begin, end){
  		if((begin !== end) && (firstIndex < lastIndex)){
  			return true;
  		}
- 		
  	}
  	function checkConditionTwo(){
  		if((begin.length === 1) && (end.length === 1)){
  			return true;
  		}
- 		
  	}
  	function checkConditionThree(){
  		//if((firstIndex >=0) && (lastIndex>=0)){
  		if((text.includes(begin)) && (text.includes(end))){
  			return true;
- 		}
- 		
+ 		}		
  	}
  	
-
  	if (checkConditionOne() && checkConditionTwo() && checkConditionThree()){
  		console.log(text.substring((firstIndex+1), lastIndex));
  	}
