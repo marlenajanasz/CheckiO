2961d3ca48a73368ae7d1b6efabc6bf9cf4f403e
Marlena Janasz <mar.janasz87@gmail.com>
2020-05-19 23:42:19 +0200

    Add files via upload

---
+++ second_index.js
@@ -0,0 +1,39 @@
+//my solution
+
+function secondIndex(str, ind){
+  let result = str.indexOf(ind, str.indexOf(ind)+1);  
+  if(result < 0){  
+      return undefined;
+    }
+    else {return result;
+    }
+ 
+//the conditional (ternary) operator
+
+function secondIndex(str, ind){
+  let result = str.indexOf(ind, str.indexOf(ind)+1);  
+  return result < 0 ? undefined : result;
+}
+
+
+//best solution
+
+let secondIndex = (t, s) => t.replace(s, '').indexOf(s) + 1 || undefined;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
